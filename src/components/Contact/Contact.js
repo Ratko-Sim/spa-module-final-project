3c87aa66f2328607ff0b1ecb09a6209ace659b98
Ratko S <73665187+Ratko-Sim@users.noreply.github.com>
2021-06-27 20:21:05 +0200

    added contact us page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import "./Contact.scss"
+import { motion } from "framer-motion"
 
 function Contact() {
   return (
 
-    <div className="Contact">
+    <motion.div className="Contact"
+      exit={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}>
       <div className="Contact-top-container">
         <h1>do you have some questions?</h1>
       </div>
-      <div className="Contact-left">
 
+      <div className="Contact-card">
+        <input type="text" placeholder="Enter your name..." />
+        <input type="text" placeholder="Enter your email..." />
+        <textarea name="" id="" cols="30" rows="10" placeholder="Enter your comment..."></textarea>
+        <button type="submit">submit</button>
       </div>
-      <div className="Contact-right">
-        <input type="text" />
-        <input type="text" />
-        <textarea name="" id="" cols="30" rows="10"></textarea>
-      </div>
-    </div>
+    </motion.div>
   )
 }
 
